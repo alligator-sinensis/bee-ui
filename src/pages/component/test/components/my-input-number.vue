e7c742b31a26c8a77043fbb257bf53b0237d3363
yangyu <123456@qq.com>
2024-06-17 15:55:53 +0800

    1

--- src/pages/component/test/components/my-input-number.vue
+++ src/pages/component/test/components/my-input-number.vue
@@ -10,6 +10,7 @@
 import { computed, nextTick, reactive, ref, watch } from "vue"
 import isNumber from "is-number"
 import { isNil } from "lodash-es"
+import { watchPausable } from "@vueuse/core"
 
 const props = withDefaults(
   defineProps<{
@@ -21,33 +22,29 @@ const emit = defineEmits(["update:modelValue"])
 
 const displayValue = ref("")
 
-const isPauseWatchModelValue = ref(false)
+const { pause: PauseWatchModelValue, resume: resumeWatchModelValue } = watchPausable(
+  () => props.modelValue,
+  async (value) => {
+    console.log("watch => modelValue", value)
+    displayValue.value = isNumber(value) ? String(value) : ""
+    emit("update:modelValue", displayValue.value === "" ? null : Number(displayValue.value))
+  },
+  {
+    immediate: true,
+  },
+)
 
 const onInput = async (event) => {
   const { value } = event.detail
-  console.log("onInput", value)
-  isPauseWatchModelValue.value = true
-  await setVerifyValue(value)
+  PauseWatchModelValue()
+  await setVerifyValueByDisplayValue(value)
   emitModelValue()
   await nextTick()
-  isPauseWatchModelValue.value = false
-
-  //   // 设置成 v-model 就不需要这一步了
-  //   // displayValue.value = value
-  //   const verifyValue = getVerifyValue(value)
-  //   console.log({ verifyValue, displayValue: displayValue.value })
-  //   if (verifyValue !== displayValue.value) {
-  //     await nextTick()
-  //     displayValue.value = verifyValue
-  //   }
+  resumeWatchModelValue()
 }
 
-const setVerifyValue = async (value: string) => {
-  console.log("setVerifyValue", value)
-  if (value === "-") {
-    displayValue.value = value
-    return
-  }
+async function setVerifyValueByDisplayValue(value: string) {
+  console.log("setVerifyValueByDisplayValue", value)
   if (isNumber(value)) {
     displayValue.value = value
     return
@@ -61,64 +58,16 @@ const setVerifyValue = async (value: string) => {
     : String(parseFloatValue)
 }
 
-const setVerifyValueByModelValue = async (value) => {
-  console.log("setVerifyValueByModelValue", value)
-  const newValue = isNil(value) ? "" : String(value)
-  if (isNumber(value)) {
-    displayValue.value = newValue
-    return
-  } else {
-    displayValue.value = ""
-  }
-}
+// async function setVerifyValueByModelValue(value) {
+//   console.log("setVerifyValueByModelValue", value)
+//   const newValue = isNil(value) ? "" : String(value)
+//   displayValue.value = isNumber(value) ? newValue : ""
+// }
 
 const emitModelValue = () => {
   const value = displayValue.value
   emit("update:modelValue", ["", "-"].includes(value) ? null : value)
 }
-
-/*
-  监听 modelValue
-  '-1w' => 格式化 => '-1' emit(-1)
-  '-w' => 格式化 => '' emit(null)
-*/
-watch(
-  () => props.modelValue,
-  async (newValue) => {
-    if (isPauseWatchModelValue.value) {
-      return
-    }
-    console.log("watch => modelValue", newValue)
-    console.log({ displayValue: displayValue.value })
-    // if (newValue === null && displayValue.value === "-") {
-    //   return
-    // }
-    await setVerifyValueByModelValue(newValue)
-    // emitModelValue()
-    emit("update:modelValue", displayValue.value === "" ? null : Number(displayValue.value))
-  },
-  {
-    immediate: true,
-  },
-)
-
-/*
-  监听 displayValue
-  '-1w' => 格式化 => '-1'  emit(-1)
-  '-w' => 格式化 => '-' emit(null)
-*/
-// watch(
-//   displayValue,
-//   async (value) => {
-//     console.log("watch => displayValue", value)
-//     await setVerifyValue(value)
-//     // console.log("setVerifyValue-end", displayValue.value)
-//     emitModelValue()
-//   },
-//   // {
-//   //   immediate: true,
-//   // },
-// )
 </script>
 
 <style scoped lang="scss">
